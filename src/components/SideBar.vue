f30cc9934ee03d78441aec232e2f359356d22bac
Safi <boolka322@gmail.com>
2020-03-10 15:59:11 +0500

    Add SideBar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="note" v-for="note in notes">
+      <p class="title">{{note.title}}</p>
+      <p class="desc">{{note.desc}}</p>
+      <span class="date">{{note.date}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['notes']
+}
+</script>
---
+++ src/views/Notes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="sidebar">
+      <side-bar :notes="notes"/>
+    </div>
+    <div class="main">
+      <h1>Главная</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar'
+
+export default {
+  components: {
+    SideBar
+  },
+  data () {
+    return {
+      notes: [
+        { title: 'Моя заметка', desc: 'Описание', body: 'Мой текст', date: (new Date).toLocaleString()  },
+         { title: 'Моя заметка 2', desc: 'Описание 2', body: 'Мой текст 2', date: (new Date).toLocaleString()  }
+      ]
+    }
+  }
+}
+</script>
