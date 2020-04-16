71619952c0d87154f3747a2e623a0c549386cfbc
Safi <boolka322@gmail.com>
2020-04-16 23:42:07 +0500

    addnotes

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,26 +1,57 @@
 <template>
   <div>
     <div class="sidebar">
-      <side-bar :notes="notes"/>
+      <div>
+        <div class="add-note" @click="addNote">
+          +
+        </div>
+        <div class="note" v-for="note, index in notes" @click="selectNote(note)">
+          <p class="title">{{note.title}}</p>
+          <p class="desc">{{note.desc}}</p>
+          <span class="delete" @click="deleteNote(note, index)">Удалить</span>
+          <span class="date">{{note.date}}</span>
+        </div>
+      </div>
     </div>
     <div class="main">
-      <h1>Главная</h1>
+      <div v-if="editNote">
+        <h1>{{editNote.title}}</h1>
+        <input v-model="editNote.title"/>
+        <textarea v-model="editNote.desc"></textarea>
+        <textarea rows="30" v-model="editNote.body"></textarea>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import SideBar from '@/components/SideBar'
-
 export default {
-  components: {
-    SideBar
+  methods: {
+    deleteNote (note, index) {
+      if (confirm('Вы действительно хотите удалить заметку?')) {
+        this.notes.splice(index, 1)
+      }
+    },
+    addNote () {
+      let note = {
+        title: 'Новая заметка',
+        desc: 'Описание',
+        body: '',
+        date: (new Date).toLocaleString()
+      }
+
+      this.notes.unshift(note)
+    },
+    selectNote (note) {
+      this.editNote = note
+    }
   },
-  data () {
+  data() {
     return {
+      editNote: null,
       notes: [
-        { title: 'Моя заметка', desc: 'Описание', body: 'Мой текст', date: (new Date).toLocaleString()  },
-         { title: 'Моя заметка 2', desc: 'Описание 2', body: 'Мой текст 2', date: (new Date).toLocaleString()  }
+        { title: 'Моя заметка', desc: 'Описание', body: 'Текст', date: (new Date).toLocaleString() },
+        { title: 'Моя заметка 2', desc: 'Описание 2', body: 'Текст 2', date: (new Date).toLocaleString() }
       ]
     }
   }
